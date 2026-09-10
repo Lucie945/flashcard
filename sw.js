d6803225f6b1683b929d42aedc35172fc0176c0b
Lucie945 <lucie.hougnon@live.fr>
2026-09-10 15:06:20 +0200

    Mise à jour Flashcard 2026-09-10 15:06

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "flashcard-20260910142048";
+const CACHE_NAME = "flashcard-20260910150619";
 const ASSET_LIST = "./offline-assets.json";
 
 self.addEventListener("install", event => {
